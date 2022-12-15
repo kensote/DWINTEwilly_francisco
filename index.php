b05ffea5dc45043db112c8930d3611deea67fbe0
SrQueso <kensote@gmail.com>
2022-12-15 00:35:22 -0300

    Update index.php
    as

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="estilos/app.css">
-    <title>Index</title>
+    <title>Index 4</title>
 </head>
 <body>
     <div class="login">
